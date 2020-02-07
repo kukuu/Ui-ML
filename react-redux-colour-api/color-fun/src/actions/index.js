95d6aa192f3e2e66061093edbd7c844316ab9c5f
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2020-02-07 09:53:51 +0000

    Updated REDUX actions index.

--- react-redux-colour-api/color-fun/src/actions/index.js
+++ react-redux-colour-api/color-fun/src/actions/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function loadColor(){
     return((dispatch)=>{
         return axios.get("http://www.colr.org/json/color/random").then((response)=>{
-            dispatch(changeColor("#"+response.data.new_color));
+            dispatch(changeColor("#"+response.data.new_color));//apending hash to string to parse generated string as color.
         })
     })
 }
